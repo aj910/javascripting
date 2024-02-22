fc1834dc3de49c51ef5edfbdf202c1cd58f2f88c
aj910 <anmolj2009@gmail.com>
2024-02-22 12:20:17 +0530

    chaining of functions example

--- javascripting/array-map.js
+++ javascripting/array-map.js
@@ -23,3 +23,20 @@ const users = [
 
 const data = users.map((user) => user.age);
 console.log(data);
+
+// chaining map & filter together: filter out firstnames where age is greater than 30. Concept known as chaining of functions
+
+const filteredData = users.filter((user) => user.age >= 30).map((user) => user.firstName)
+console.log(filteredData);
+
+// using reduce function to filter out firstnames where age is greater than 30
+
+const reduceData = users.reduce((acc, curr) => {
+   if(curr.age > 30) {
+    acc.push(curr.firstName);
+   }
+   return acc
+}, []);
+
+console.log(reduceData);
+
