102b2a1a87f91122d9a3926b7da57857190ee998
aj910 <anmolj2009@gmail.com>
2024-04-03 16:07:49 +0530

    object properties

--- javascripting/object-keys.js
+++ javascripting/object-keys.js
@@ -7,3 +7,15 @@ const car = {
 const keys = Object.keys(car); 
 
 console.log(keys);
+
+// 2 keys with the same name
+
+const obj = {
+	a: 1,
+	a: 1,
+	b: 2,
+	b: 4,
+	a: 31,
+};
+
+console.log(obj)
--- javascripting/object-properties.js
+++ javascripting/object-properties.js
@@ -3,3 +3,18 @@ const food = {
 }; 
 
 console.log(food.types);
+
+// iterating through object properties or keys and displaying their values
+
+const user = {
+	name: "Anmol",
+	age: 28
+};
+
+for(key in user) {
+	console.log(key); // displays key or property name in the object
+}
+
+for(key in user) {
+	console.log(user[key]); // displays value of each property in an object
+}
